58914786e1d56100f8eff4f0dea346e00e152197
Houssame Rahiche <houssamerahiche@gmail.com>
2021-05-12 07:14:42 +0200

    first commit ps : code isnt working successfuly for unidentified reason - not showing errors nor redirecting to success page

---
+++ server.js
@@ -0,0 +1,61 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const users = []
+
+app.set('view-engine', 'ejs')
+const initializePassport =require('./passport_config')
+initializePassport(passport, (email)=>{
+   return users.find(user => user.email===email)
+},id=> users.find(user => user.id===id) )
+
+
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login.ejs')
+})
+app.post('/login',passport.authenticate('local',{
+    successRedirect : '/',
+    failureRedirect : './login',
+    failureFlash : true
+}))
+app.get('/register',(req,res)=>{
+    res.render('register.ejs')
+})
+app.post('/register',async (req,res)=>{
+   try {
+       const hashedpassword = await bcrypt.hash(req.body.password, 10)
+       users.push({
+        id: Date.now().toString(),
+        name : req.body.name,
+        password : hashedpassword
+       })
+       res.redirect('./login')
+   } catch{
+       res.redirect('./register')
+   }
+   console.log(users);
+})
+
+app.listen(3000)
